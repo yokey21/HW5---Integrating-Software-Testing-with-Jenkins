6e7a61a319a5bdc3bcf8787c764f25648d1cf140
Adam Byron <49727270+yokey21@users.noreply.github.com>
2023-04-30 17:43:32 -0700

    updated test

--- test_weather.py
+++ test_weather.py
@@ -8,18 +8,22 @@ def test_add():
 
 
 def test_analyze_weather_data():
-    weather_data = [{'city': 'New York', 'temperature': 10, 'condition': 'Sunny'},
-                    {'city': 'London', 'temperature': 15, 'condition': 'Cloudy'},
-                    {'city': 'Tokyo', 'temperature': 20, 'condition': 'Rainy'},
-                    {'city': 'Paris', 'temperature': 25,
-                        'condition': 'Thunderstorms'},
-                    {'city': 'Moscow', 'temperature': 30, 'condition': 'Snowy'}]
+    # Generate sample data
+    weather_data = generate_weather_data(5)
 
-    results = analyze_weather_data(weather_data)
+    # Calculate expected results
+    temperatures = [data['temperature'] for data in weather_data]
+    humidity = [data['humidity'] for data in weather_data]
+    windspeed = [data['windspeed'] for data in weather_data]
 
-    assert round(results['temperature_mean'], 2) == 20.0
-    assert round(results['temperature_stdev'], 2) == 7.07
-    assert round(results['humidity_mean'], 2) == 0.0
-    assert round(results['humidity_stdev'], 2) == 0.0
-    assert round(results['windspeed_mean'], 2) == 0.0
-    assert round(results['windspeed_stdev'], 2) == 0.0
+    expected_results = {
+        'temperature_mean': statistics.mean(temperatures),
+        'temperature_stdev': statistics.stdev(temperatures),
+        'humidity_mean': statistics.mean(humidity),
+        'humidity_stdev': statistics.stdev(humidity),
+        'windspeed_mean': statistics.mean(windspeed),
+        'windspeed_stdev': statistics.stdev(windspeed),
+    }
+
+    # Test the function and compare results
+    assert analyze_weather_data(weather_data) == expected_results
