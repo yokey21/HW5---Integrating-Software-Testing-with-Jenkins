8c4fa6782957236b9f92508eddc43f17c6925581
Adam Byron <49727270+yokey21@users.noreply.github.com>
2023-04-30 17:16:43 -0700

    test function

--- test_weather.py
+++ test_weather.py
@@ -5,3 +5,17 @@ from weather import *
 
 def test_add():
     assert add(2, 3) - - 5
+
+
+def test_generate_weather_data():
+    data = generate_weather_data()
+    assert isinstance(data, list)
+    assert len(data) == 10
+    for item in data:
+        assert isinstance(item, dict)
+        assert set(item.keys()) == {
+            'city', 'temperature', 'description', 'humidity'}
+        assert isinstance(item['city'], str)
+        assert isinstance(item['temperature'], int)
+        assert isinstance(item['description'], str)
+        assert isinstance(item['humidity'], int)
--- weather.py
+++ weather.py
@@ -1,2 +1,25 @@
+import random
+
+
+def generate_weather_data(num_entries):
+    weather_data = []
+    cities = ['New York', 'London', 'Tokyo', 'Paris', 'Moscow']
+    weather_conditions = ['Sunny', 'Cloudy', 'Rainy', 'Thunderstorms', 'Snowy']
+
+    for i in range(num_entries):
+        city = random.choice(cities)
+        temperature = round(random.uniform(-10, 40), 2)
+        condition = random.choice(weather_conditions)
+
+        data = {'city': city, 'temperature': temperature, 'condition': condition}
+        weather_data.append(data)
+
+    return weather_data
+
+
 def add(x, y):
     return x + y
+
+
+data = generate_weather_data(5)
+print(data)
